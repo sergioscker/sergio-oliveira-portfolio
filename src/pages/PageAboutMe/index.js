5abe2af077b4a9cfa110287e06407212f86951e1
sergioliveira-developer <sergiowallace11@hotmail.com>
2024-07-25 23:43:14 +0100

    Mudança na descrição do "sobre"

--- src/pages/PageAboutMe/index.js
+++ src/pages/PageAboutMe/index.js
@@ -1,30 +1,38 @@
-import { ContainerAbout } from './styles';
+import { ContainerAbout } from "./styles";
 
 export const PageAboutMe = () => {
-
   return (
     <ContainerAbout>
       <div>
-
-        <h5 className='text'>
-          Olá, 👋 me chamo Sérgio Oliveira!
-          Minha trajetória é marcada pela aplicação das tecnologias mais atuais, como React, TypeScript e JavaScript, para criar soluções de alta qualidade.
-          Minhas habilidades técnicas abrangem uma ampla gama de tecnologias, incluindo JavaScript, ReactJS, TypeScript, HTML, CSS, Styled Components.
-        </h5>
-
-        <h5 className='text'>
-          Atualmente, atuo como Desenvolvedor Full-Stack em projetos Freelance, entregando resultados de excelência.Além disso, contribuo em diversas frentes, incluindo programação, apoio em atividades de infraestrutura, participação e suporte em treinamentos internos, auxílio em atividades de testes e garantia de qualidade de software.
-          Tenho sólidos conhecimentos em noções de lógica de programação, SQL e NoSQL, que me permitem abordar os desafios com eficiência. <br />
-          <br />
-          Adicionalmente, estou continuamente me aprimorando e atualizando meu conhecimento para garantir que minhas habilidades estejam sempre alinhadas com as mais recentes tendências da indústria.
+        <h5 className="text">
+          Olá, 👋 me chamo Sérgio Oliveira! Minha trajetória é marcada pela
+          aplicação das tecnologias mais atuais, para criar soluções de alta
+          qualidade, como: JavaScript, ReactJS, TypeScript, HTML5, CSS3, entre
+          outras.
           <br />
         </h5>
 
-        <h5 className='text'>
-          Estou comprometido com o aprendizado contínuo e o aprimoramento das minhas habilidades como desenvolvedor. Além de buscar constantemente novos conhecimentos e técnicas em cursos online, workshops e eventos da área, também faço parte de comunidades de desenvolvedores, onde posso compartilhar experiências, aprender com outros profissionais e contribuir para o crescimento coletivo.
+        <h5 className="text">
+          Atuo como Desenvolvedor Full-Stack em projetos Freelance, entregando
+          resultados de excelência. Além disso, contribuo em alguns segmentos,
+          incluindo desenvolvimento, auxílio em atividades de testes e
+          manutenção para garantia de qualidade de software. Adicionalmente,
+          estou continuamente me aprimorando e atualizando meus conhecimentos
+          para garantir que minhas habilidades estejam sempre alinhadas com as
+          mais recentes tendências tecnológicas da indústria.
+          <br />
         </h5>
 
+        <h5 className="text">
+          Estou comprometido com o aprendizado contínuo e o aprimoramento das
+          minhas habilidades técnicas como desenvolvedor. Busco constantemente
+          novos conhecimentos e técnicas em cursos online, workshops e eventos
+          da área, também faço parte de comunidades de desenvolvedores, onde
+          posso compartilhar experiências, aprender com outros profissionais e
+          contribuir para o crescimento coletivo.
+          <br />
+        </h5>
       </div>
     </ContainerAbout>
-  )
+  );
 };
